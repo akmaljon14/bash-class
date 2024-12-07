e34236121cf7023a12bd235b2020f595a42cb93a
EC2 Default User <ec2-user@ip-172-31-83-16.ec2.internal>
2024-12-07 02:51:22 +0000

    Class 3

---
+++ class3/for.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+for i in 1 2 3 4 5 67 
+do
+	echo $i
+done
---
+++ class3/user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sudo groupadd devops
+
+for user in tim eric brad ann jenny jessica brian
+do
+	sudo useradd $user
+	sudo usermod -aG $user
+done 
